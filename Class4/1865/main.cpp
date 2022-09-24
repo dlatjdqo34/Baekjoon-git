db4767f0da1a443f80028eaee4e06af61a2cd000
dlatjdqo34 <isb9000@naver.com>
2022-09-25 00:09:04 +0900

    Refresh

---
+++ Class4/1865/main.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int maxD = 100000000;
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+  int TC;
+  cin >> TC;
+  for (int i = 0; i < TC; i++) {
+    int N, M;
+    
+  }
+}
